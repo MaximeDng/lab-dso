3799ea978a7ead19b57439d632fe0e4acef82002
MaximeD <donge.maxime@gmail.com>
2026-02-15 17:57:05 +0100

    ajout gitleaks dans workflow et test dans le code

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var DB_PASSWORD = "EANFISNCWW?WLO?CPO?W24552%%$";
+
 var app = express();
 
 // view engine setup
