0905f50af9885159e349d80c1f07295385261bb1
MaximeD <donge.maxime@gmail.com>
2026-02-15 18:02:38 +0100

    tst password leak

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var Password = "esd";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+function SetPassword() {
+    Password =  "EANFISNCWW?WLO?CPO?W24552%%$";
+}
 
 module.exports = router;
