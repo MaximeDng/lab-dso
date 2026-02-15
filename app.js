9835b4a56eeff34433d3a1c736d7c988b0e3cea2
Maxime Dongé <donge.maxime@gmail.com>
2026-02-15 18:16:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var AWS_SECRET_KEY = "AKIAIMNO88SA5EXAMPLE";
+var AWS_SECRET_KEY = "AKIAIMNO88SA5EXAMPLE";
 
 var app = express();
 
